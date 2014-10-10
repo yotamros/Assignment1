eaeb909b839c1cece4482ea373387026689ba790
Yotam Rosenbaum <yotamrosen@gmail.com>
2014-10-10 09:52:08 -0500

    Initial Commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Yotam Rosenbaum on 10/10/14.
+//  Copyright (c) 2014 yotam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
